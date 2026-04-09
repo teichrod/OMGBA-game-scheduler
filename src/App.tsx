2c1383dea4a4195775acf4f91e913dfb85e8c850
Darius Teichroew <dteichroew@air-quality-eng.com>
2026-04-09 16:31:12 -0500

    fix React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { CalendarDays, Trophy, Settings, AlertTriangle, Wand2, Download, Building2 } from "lucide-react";
 
 const DIVISIONS = [
